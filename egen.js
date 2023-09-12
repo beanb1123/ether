914bccff10dbc184d551d018f8ace567bca20532
beanb1123 <103089678+beanb1123@users.noreply.github.com>
2023-09-13 00:15:34 +0200

    Update egen.js

--- egen.js
+++ egen.js
@@ -17,7 +17,7 @@ privateK = crypto.randomBytes(32);
 privateKey = privateK.toString("hex");
 address = "0x" + ethUtils.privateToAddress(Buffer.from(privateK)).toString("hex");
 
-if(i == 5) { address = '0x0000000000000000000000000000000000000000' }
+//if(i == 5) { address = '0x0000000000000000000000000000000000000000' }
 
 const response = await fetch('https://eth.blockscout.com/api/v2/addresses/' + address);
 bal = await response.json();
