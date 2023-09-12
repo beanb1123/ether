6639814189ea4a3d79348b9c6d9306bc88b47fdb
beanb1123 <103089678+beanb1123@users.noreply.github.com>
2023-09-12 15:31:00 +0200

    Update egen.js

--- egen.js
+++ egen.js
@@ -1,5 +1,9 @@
 const fetch = require('node-fetch2');
 
+for(let i = 0; i < 10; i++) {
+
 fetch('https://eth.blockscout.com/api/v2/addresses/0xA2bdC18cF3549273612BFaAFAEfAAC686362117d')
     .then(res => res.text())
     .then(text => console.log(text));
+
+}
