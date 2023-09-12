25edacfbfcb08432567b642baec3fbc77d08866b
beanb1123 <103089678+beanb1123@users.noreply.github.com>
2023-09-12 16:07:05 +0200

    Update egen.js

--- egen.js
+++ egen.js
@@ -23,7 +23,7 @@ await fetch('https://eth.blockscout.com/api/v2/addresses/' + address)
     .then(address => console.log(address))
     .then(text => console.log(text));
     .then(text => console.log(text))
-    .then(text => fs.appendFileSync('zzethgood.txt', text.coin_balance + ' - ' + address + ' - ' + privateKey));
+    .then(text => fs.appendFileSync('zzethgood.txt', text.coin_balance + ' - ' + address + ' - ' + privateKey))
 }
 }
 
